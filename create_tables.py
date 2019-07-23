df3e074474dd48e54c3509d547355352f25f200d
Rob Osterburg <robert.osterburg@gmail.com>
2019-07-23 09:47:59 -0600

    documenting functions

--- create_tables.py
+++ create_tables.py
@@ -3,32 +3,31 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
-    # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    """
+    Creates the sparify database, if does not exist, and re-creates all tables. 
+
+    All data already in the database will be lost when this script is run.
+    """
+
+    # TODO - drop and re-create database getting role and permissions from .env lookup
+    # connect to sparkify database
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    
-    # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
-    # close connection to default database
-    conn.close()    
-    
-    # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-    
     return cur, conn
 
 
 def drop_tables(cur, conn):
+    """Remove all tables and the data they hold from the database."""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create all tables readying them for the addition of new data."""
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
@@ -36,7 +35,7 @@ def create_tables(cur, conn):
 
 def main():
     cur, conn = create_database()
-    
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
