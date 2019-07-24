cb7dce552c0d55bd83f7a7c7dad5386ac6bc0156
Rob Osterburg <robert.osterburg@gmail.com>
2019-07-24 11:09:39 -0600

    edit write up

--- etl_prototype.py
+++ etl_prototype.py
@@ -1,6 +1,6 @@
 # %% [markdown]
-# # ETL Processes
-# Use this notebook to develop the ETL process for each of your tables before completing the `etl.py` file to load the whole datasets.
+# # ETL Prototype
+# Establish data processing workflow for a small subset of the data.
 
 # %%
 import os
@@ -11,6 +11,9 @@ import numpy as np
 from pathlib import Path
 from sql_queries import *
 
+# %% [markdown]
+# ## Connect to Postgres Database
+# After connecting to the database and getting a cursor object, then drop and recreate all tables.
 
 # %%
 conn = psycopg2.connect(
@@ -18,10 +21,21 @@ conn = psycopg2.connect(
 )
 conn.set_session(autocommit=True)
 cur = conn.cursor()
+for sql_cmd in drop_table_queries + create_table_queries:
+    cur.execute(sql_cmd)
 
+# %% [markdown]
+# ## Find data files for processing
+#
+# Use `os.walk` to find all `*.json` files under the `filepath` directory.
 
 # %%
+# Let's apply the DRY principle and write a function to load our
+# data.
+
+
 def get_files(filepath):
+    """Return all JSON files under filepath as a list"""
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root, "*.json"))
@@ -32,13 +46,10 @@ def get_files(filepath):
 
 
 # %% [markdown]
-# # Process `song_data`
-# In this first part, you'll perform ETL on the first dataset, `song_data`, to create the `songs` and `artists` dimensional tables.
+# ## #1: `song` Table
+# #### Extract Data for Song Table
 #
-# Let's perform ETL on a single song file and load a single record into each table to start.
-# - Use the `get_files` function provided above to get a list of all song JSON files in `data/song_data`
-# - Select the first song in this list
-# - Read the song file and view the data
+# Process `song_data` by reading in a subset of the [Million Song Dataset](http://millionsongdataset.com/) and in the process extracting data from JSON files using pandas.
 
 # %%
 song_root_dir = Path().cwd() / "data" / "song_data"
@@ -47,64 +58,39 @@ filepath = song_files[0]
 df = pd.read_json(filepath, lines=True)
 
 # %% [markdown]
-# ## #1: `songs` Table
-# #### Extract Data for Songs Table
-# - Select columns for song ID, title, artist ID, year, and duration
-# - Use `df.values` to select just the values from the dataframe
-# - Index to select the first (only) record in the dataframe
-# - Convert the array to a list and set it to `song_data`
+# #### Insert Data into the Song Table
+#
+# - Method 1: select columns and return as a tuple knowing that there is one song per dataframe and results in __year as typye np.int64 and duration as type np.float64__.  Pandas uses numpy to store its numeric types, so this result is expected.
 
 # %%
-# alternate method: select columns and return as a tuple knowing that there is one song per dataframe
-# note: results in year as typye np.int64 and duration as type np.float64
 song_data = next(
     df[["song_id", "title", "artist_id", "year", "duration"]].itertuples(
         index=False, name=None
     )
 )
-song_data
-
-# %%
-# recommended method: select columns, select first row, get values as numpy array and convert to a list
-# note: results in year as typye int and duration as type float
-song_data = df[["song_id", "title", "artist_id",
-                "year", "duration"]].values[0].tolist()
-song_data
-
-# alternative that results in np.int64 and np.float64 numeric types apparently due to using iloc
-# see: https://knowledge.udacity.com/questions/38150
-# song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].iloc[0, :].values.tolist()
-# %%
-# drop and re-create all tables
-for sql_cmd in drop_table_queries + create_table_queries:
-    cur.execute(sql_cmd)
 
 # %% [markdown]
-# #### Insert Record into Song Table
-# Implement the `song_table_insert` query in `sql_queries.py` and run the cell below to insert a record for this song into the `songs` table. Remember to run `create_tables.py` before running the cell below to ensure you've created/resetted the `songs` table in the sparkify database.
+# - Method 2: Select columns, select first row, get values as numpy array and convert to a list that results in __year as typye int and duration as type float__.  Inserting numpy numeric types into the database using psycopg2 causes errors, so I will use this approach.  This type conversion occurs because it is behavior of [numpy.ndarray.tolist](https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.tolist.html#numpy.ndarray.tolist) upon which [pandas.Series.tolist](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.tolist.html) is based.  Mystery solved!
 
 # %%
-# how should I handle missing values?  Here is one way:
-# replace missing values with None since pd.read_json does not handle missing value conversion
+# Select and insert data into the songs table
+song_df = df[["song_id", "title", "artist_id",
+              "year", "duration"]]
+song_df.head()
+
+# %%
+song_data = song_df.values[0].tolist()
 song_data = [x if x else None for x in song_data]
-
-# %%
 cur.execute(song_table_insert, song_data)
 
-
-# %% [markdown]
-# Run `test.ipynb` to see if you've successfully added a record to this table.
 # %% [markdown]
 # ## #2: `artists` Table
-# #### Extract Data for Artists Table
-# - Select columns for artist ID, name, location, latitude, and longitude
-# - Use `df.values` to select just the values from the dataframe
-# - Index to select the first (only) record in the dataframe
-# - Convert the array to a list and set it to `artist_data`
+# #### Extract Data for Artist Table
+#
+# Extract data and insert into artist table.
 
 # %%
-print(df.columns)
-artist_data = (
+artist_df = (
     df[
         [
             "artist_id",
@@ -114,60 +100,36 @@ artist_data = (
             "artist_longitude",
         ]
     ]
-    .values[0]
-    .tolist()
 )
-artist_data
-
-# %% [markdown]
-# #### Insert Record into Artist Table
-# Implement the `artist_table_insert` query in `sql_queries.py` and run the cell below to insert a record for this song's artist into the `artists` table. Remember to run `create_tables.py` before running the cell below to ensure you've created/resetted the `artists` table in the sparkify database.
+artist_df.head()
 
 # %%
+artist_data = artist_df.values[0].tolist()
 cur.execute(artist_table_insert, artist_data)
 
 # %% [markdown]
-# Run `test.ipynb` to see if you've successfully added a record to this table.
-# %% [markdown]
 # # Process `log_data`
-# In this part, you'll perform ETL on the second dataset, `log_data`, to create the `time` and `users` dimensional tables, as well as the `songplays` fact table.
 #
-# Let's perform ETL on a single log file and load a single record into each table.
-# - Use the `get_files` function provided above to get a list of all log JSON files in `data/log_data`
-# - Select the first log file in this list
-# - Read the log file and view the data
+# Now let's add the subscriber activity data to see which songs are popular.
 
 # %%
 log_data_root = Path().cwd() / "data" / "log_data"
 log_files = get_files(log_data_root)
-len(log_files)
-
-
-# %%
+# just read first file to test functionality
 filepath = log_files[0]
-
-# %%
 df = pd.read_json(filepath, lines=True)
-df = df.assign(ts=pd.to_datetime(df.ts, unit="ms"))
-df = df.loc[df.page.isin(["NextSong"])]
 
 # %% [markdown]
 # ## #3: `time` Table
-# #### Extract Data for Time Table
-# - Filter records by `NextSong` action
-# - Convert the `ts` timestamp column to datetime
-#   - Hint: the current timestamp is in milliseconds
-# - Extract the timestamp, hour, day, week of year, month, year, and weekday from the `ts` column and set `time_df` to a list containing these values in order
-#   - Hint: use pandas' [`dt` attribute](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.dt.html) to access easily datetimelike properties.
-# - Specify labels for these columns and set to `column_labels`
-# - Create a dataframe, `time_data,` containing the time data for this file by combining `column_labels` and `time_data` into a dictionary and converting this into a dataframe
+# #### Extract and Insert Data into Time Table
+#
+# Find what songs user's are choosing by just considering `NextSong` records.  Then convert the `ts` timestamp column to datetime and extract columns for hour, day, week of year, month, year, and weekday (see: [Accessors](https://pandas.pydata.org/pandas-docs/stable/reference/series.html#time-series-related) [dt Accessor](https://pandas.pydata.org/pandas-docs/stable/reference/series.html#api-series-dt) that allows datetime properties to be easily accessed).
 
 # %%
-
-# cite: https://pandas.pydata.org/pandas-docs/stable/reference/series.html#time-series-related
+df = df.assign(ts=pd.to_datetime(df.ts, unit="ms"))
+df = df.loc[df.page.isin(["NextSong"])]
 df = df.assign(timestamp=pd.to_datetime(df.ts, unit="ms"))
 df.timestamp = df.timestamp.dt.tz_localize("UTC")
-df.head(10)
 
 # %%
 time_df = pd.DataFrame(
@@ -181,51 +143,40 @@ time_df = pd.DataFrame(
         "weekday": df.timestamp.dt.weekday,
     }
 )
-
-# %% [markdown]
-# #### Insert Records into Time Table
-# Implement the `time_table_insert` query in `sql_queries.py` and run the cell below to insert records for the timestamps in this log file into the `time` table. Remember to run `create_tables.py` before running the cell below to ensure you've created/resetted the `time` table in the sparkify database.
-
-# %%
+# Here we want native pandas datatypes, so I'll user iterrows.
 for i, row in time_df.iterrows():
     cur.execute(time_table_insert, list(row))
 
-# %% [markdown]
-# Run `test.ipynb` to see if you've successfully added records to this table.
+# %%
+time_df.head()
+
 # %% [markdown]
 # ## #4: `users` Table
-# #### Extract Data for Users Table
-# - Select columns for user ID, first name, last name, gender and level and set to `user_df`
-
-# %%
-df.columns
-
+# #### Extract and Insert Data into Users Table
+#
+# Every time a user plays a song they appear in the log file, so naturally there will by duplicate userId entries.  Here we remove them to create a normalized user table.
 # %%
 user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 user_df = user_df.drop_duplicates(subset="userId", keep="last")
-
-# %% [markdown]
-# #### Insert Records into Users Table
-# Implement the `user_table_insert` query in `sql_queries.py` and run the cell below to insert records for the users in this log file into the `users` table. Remember to run `create_tables.py` before running the cell below to ensure you've created/resetted the `users` table in the sparkify database.
+user_df.head()
 
 # %%
 for i, row in user_df.iterrows():
     cur.execute(user_table_insert, row)
 
 # %% [markdown]
-# Run `test.ipynb` to see if you've successfully added records to this table.
-
-# %% [markdown]
 # ## #5: `songplays` Table
-# #### Extract Data and Songplays Table
-# This one is a little more complicated since information from the songs table, artists table, and original log file are all needed for the `songplays` table. Since the log file does not specify an ID for either the song or the artist, you'll need to get the song ID and artist ID by querying the songs and artists tables to find matches based on song title, artist name, and song duration time.
+# #### Extract and Insert Data and Songplays Table
 #
-# - Implement the `song_select` query in `sql_queries.py` to find the song ID and artist ID based on the title, artist name, and duration of a song.
+# To look up song or an artist, I need the unique identifier or primary key. The log files simply have the name of the song and artist.  So, I need to do a reverse lookup up to get identifiers.
 #
-# - Select the timestamp, user ID, level, song ID, artist ID, session ID, location, and user agent and set to `songplay_data`
+# ```sql
+# SELECT s.song_id, a.artist_id FROM dim_song s
+# JOIN dim_artist a ON s.artist_id = a.artist_id
+# WHERE s.title = %s AND a.name = %s AND s.duration = %s;
+# ```
 #
-# #### Insert Records into Songplays Table
-# - Implement the `songplay_table_insert` query and run the cell below to insert records for the songplay actions in this log file into the `songplays` table. Remember to run `create_tables.py` before running the cell below to ensure you've created/resetted the `songplays` table in the sparkify database.
+# Iterating over the rows of the dataframe holding the log data.  First, I extract the find the unique identifiers, Next, I combine them with other data from the log data to insert the user's songplay activity into the `song_play` table.
 
 # %%
 for index, row in df.iterrows():
@@ -253,13 +204,7 @@ for index, row in df.iterrows():
     cur.execute(songplay_table_insert, songplay_data)
 
 # %% [markdown]
-# Run `test.ipynb` to see if you've successfully added records to this table.
-# %% [markdown]
-# # Close Connection to Sparkify Database
+# ## Close Connection to Sparkify Database
 
 # %%
 conn.close()
-
-# %% [markdown]
-# # Implement `etl.py`
-# Use what you've completed in this notebook to implement `etl.py`.
